b2e939fd9f5865f3af29326b56cd00731cdf1696
Jason Paterson <jasonbipaterson@gmail.com>
2021-12-03 17:49:13 +0000

    Refactor day 3 2021

--- 2021/src/bin/day03.rs
+++ 2021/src/bin/day03.rs
@@ -1,78 +1,63 @@
-use std::fs;
+use std::{cmp, fs};
+
+fn common_bit(words: &[&str], i: usize) -> cmp::Ordering {
+    let mut count = 0;
+    for &word in words {
+        match &word.chars().nth(i) {
+            Some('1') => count += 1,
+            Some('0') => count -= 1,
+            _ => panic!("Unexpected char"),
+        };
+    }
+    count.cmp(&0)
+}
 
 fn part1(words: &[&str]) -> u32 {
     let len = words[0].chars().count();
-    let mut bit_count: Vec<i32> = vec![0; len];
-    for word in words {
-        for (i, c) in word.chars().enumerate() {
-            match c {
-                '1' => bit_count[i] += 1,
-                '0' => bit_count[i] -= 1,
-                _ => panic!("Unexpected char"),
-            };
+    let mut gamma: String = "".to_string();
+    let mut epsilon: String = "".to_string();
+    for i in 0..len {
+        match common_bit(words, i) {
+            cmp::Ordering::Less => {
+                gamma.push('0');
+                epsilon.push('1');
+            }
+            cmp::Ordering::Greater => {
+                gamma.push('1');
+                epsilon.push('0');
+            }
+            cmp::Ordering::Equal => panic!(),
         }
     }
-    let mut gamma: Vec<char> = vec!['0'; len];
-    let mut epsilon: Vec<char> = vec!['1'; len];
-    bit_count.iter().enumerate().for_each(|(i, n)| {
-        if *n > 0 {
-            gamma[i] = '1';
-            epsilon[i] = '0';
-        }
-    });
-    let gamma = u32::from_str_radix(&gamma.iter().collect::<String>(), 2).unwrap();
-    let epsilon = u32::from_str_radix(&epsilon.iter().collect::<String>(), 2).unwrap();
+    let gamma = u32::from_str_radix(&gamma, 2).unwrap();
+    let epsilon = u32::from_str_radix(&epsilon, 2).unwrap();
     gamma * epsilon
 }
 
-fn part2(words: &[&str]) -> u32 {
+fn find_rating(words: &[&str], bit_match: (char, char, char)) -> u32 {
     let len = words[0].chars().count();
     let mut remaining_words = words.to_vec();
     for i in 0..len {
-        let mut count = 0;
-        remaining_words.iter().for_each(|&word| {
-            match &word.chars().nth(i) {
-                Some('1') => count += 1,
-                Some('0') => count -= 1,
-                _ => panic!("Unexpected char"),
-            };
-        });
+        let bit_char = match common_bit(&remaining_words, i) {
+            cmp::Ordering::Less => bit_match.0,
+            cmp::Ordering::Equal => bit_match.1,
+            cmp::Ordering::Greater => bit_match.2,
+        };
         remaining_words = remaining_words
             .iter()
-            .filter(|&word| match word.chars().nth(i) {
-                Some('1') => count >= 0,
-                Some('0') => count < 0,
-                _ => panic!(),
-            })
+            .filter(|&word| word.chars().nth(i) == Some(bit_char))
             .map(|m| *m)
-            .collect();
-    }
-    let oxygen = u32::from_str_radix(remaining_words[0], 2).unwrap();
-
-    let mut remaining_words = words.to_vec();
-    for i in 0..len {
-        let mut count = 0;
-        remaining_words.iter().for_each(|&word| {
-            match &word.chars().nth(i) {
-                Some('1') => count += 1,
-                Some('0') => count -= 1,
-                _ => panic!("Unexpected char"),
-            };
-        });
-        remaining_words = remaining_words
-            .iter()
-            .filter(|&word| match word.chars().nth(i) {
-                Some('1') => count < 0,
-                Some('0') => count >= 0,
-                _ => panic!(),
-            })
-            .map(|m| *m)
-            .collect();
+            .collect::<Vec<&str>>();
         if remaining_words.len() == 1 {
             break;
         }
     }
-    let carbon_dioxide = u32::from_str_radix(remaining_words[0], 2).unwrap();
+    u32::from_str_radix(remaining_words[0], 2).unwrap()
+}
+
+fn part2(words: &[&str]) -> u32 {
+    let oxygen = find_rating(&words, ('0', '1', '1'));
+    let carbon_dioxide = find_rating(&words, ('1', '0', '0'));
     oxygen * carbon_dioxide
 }
 
