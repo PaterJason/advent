f0f596c0b1d1406e35b10bf1850f639b3cdb1d82
Jason Paterson <jasonbipaterson@gmail.com>
2021-12-07 14:26:12 +0000

    Day 06 2021

---
+++ 2021/src/bin/day06.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+fn stepn(fish_timers: &[u64; 9], n: u64) -> u64 {
+    let mut timers = fish_timers.clone();
+    for _ in 0..n {
+        let tmp_timers = timers.clone();
+        timers = [0u64; 9];
+        for (i, n) in tmp_timers.iter().enumerate() {
+            match i {
+                0 => {
+                    timers[6] += *n;
+                    timers[8] += *n
+                }
+                _ => timers[i - 1] += *n,
+            }
+        }
+    }
+    timers.iter().sum()
+}
+
+fn parse_input(input: &str) -> [u64; 9] {
+    let mut fish_timers = [0u64; 9];
+    input
+        .trim()
+        .split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|i| fish_timers[i] += 1);
+    fish_timers
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input/day06").expect("Failed to read input");
+
+    let fish_timers = parse_input(&input);
+
+    println!("Part 1: {}", stepn(&fish_timers, 80));
+    println!("Part 2: {}", stepn(&fish_timers, 256));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "3,4,3,1,2";
+
+    #[test]
+    fn test_part1() {
+        let timers = parse_input(&INPUT);
+        assert_eq!(stepn(&timers, 18), 26);
+        assert_eq!(stepn(&timers, 80), 5934);
+    }
+
+    #[test]
+    fn test_part2() {
+        let timers = parse_input(&INPUT);
+        assert_eq!(stepn(&timers, 256), 26984457539);
+    }
+}
