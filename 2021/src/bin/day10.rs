783c0f506efe302f1cc972332d8b2db2574f19d5
Jason Paterson <jasonbipaterson@gmail.com>
2021-12-10 16:00:18 +0000

    Unify both parts Day 10 2021

--- 2021/src/bin/day10.rs
+++ 2021/src/bin/day10.rs
@@ -4,46 +4,9 @@ fn parse_input(input: &str) -> Vec<&str> {
     input.trim().lines().collect()
 }
 
-fn part1(lines: &[&str]) -> u32 {
-    let mut errs: [u32; 4] = [0, 0, 0, 0];
-    for &line in lines {
-        let mut ch_stack: Vec<char> = vec![];
-        for ch in line.chars() {
-            if matches!(ch, '(' | '[' | '{' | '<') {
-                ch_stack.push(ch);
-                continue;
-            }
-            let ch_pop = ch_stack.pop();
-            match ch {
-                ')' if ch_pop == Some('(') => continue,
-                ']' if ch_pop == Some('[') => continue,
-                '}' if ch_pop == Some('{') => continue,
-                '>' if ch_pop == Some('<') => continue,
-                ')' => {
-                    errs[0] += 1;
-                    break;
-                }
-                ']' => {
-                    errs[1] += 1;
-                    break;
-                }
-                '}' => {
-                    errs[2] += 1;
-                    break;
-                }
-                '>' => {
-                    errs[3] += 1;
-                    break;
-                }
-                _ => panic!(),
-            }
-        }
-    }
-    errs[0] * 3 + errs[1] * 57 + errs[2] * 1197 + errs[3] * 25137
-}
-
-fn part2(lines: &[&str]) -> u64 {
+fn parts(lines: &[&str]) -> (u32, u64) {
     let mut scores: Vec<u64> = vec![];
+    let mut part1: u32 = 0;
     'lines: for &line in lines {
         let mut ch_stack: Vec<char> = vec![];
         for ch in line.chars() {
@@ -53,33 +16,39 @@ fn part2(lines: &[&str]) -> u64 {
             }
             let ch_pop = ch_stack.pop();
             match ch {
-                ')' if ch_pop == Some('(') => continue,
-                ']' if ch_pop == Some('[') => continue,
-                '}' if ch_pop == Some('{') => continue,
-                '>' if ch_pop == Some('<') => continue,
-                ')' | ']' | '}' | '>' => continue 'lines,
+                ')' if ch_pop != Some('(') => part1 += 3,
+                ']' if ch_pop != Some('[') => part1 += 57,
+                '}' if ch_pop != Some('{') => part1 += 1197,
+                '>' if ch_pop != Some('<') => part1 += 25137,
+                ')' | ']' | '}' | '>' => continue,
                 _ => panic!(),
             }
+            continue 'lines;
         }
-        scores.push(ch_stack.iter().rev().fold(0_u64, |score, ch| match ch {
-            '(' => score * 5 + 1,
-            '[' => score * 5 + 2,
-            '{' => score * 5 + 3,
-            '<' => score * 5 + 4,
-            _ => score,
+        scores.push(ch_stack.iter().rev().fold(0_u64, |score, ch| {
+            score * 5
+                + match ch {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => panic!(),
+                }
         }));
     }
     scores.sort_unstable();
-    scores[scores.len() / 2]
+    let part2 = scores[scores.len() / 2];
+    (part1, part2)
 }
 
 fn main() {
     let input: String = fs::read_to_string("input/day10").expect("Failed to read input");
 
     let lines = parse_input(&input);
+    let (part1, part2) = parts(&lines);
 
-    println!("Part 1: {}", part1(&lines));
-    println!("Part 2: {}", part2(&lines));
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[cfg(test)]
@@ -99,14 +68,10 @@ mod tests {
 ";
 
     #[test]
-    fn test_part1() {
+    fn test_parts() {
         let lines = parse_input(INPUT);
-        assert_eq!(part1(&lines), 26397);
-    }
-
-    #[test]
-    fn test_part2() {
-        let lines = parse_input(INPUT);
-        assert_eq!(part2(&lines), 288_957);
+        let (part1, part2) = parts(&lines);
+        assert_eq!(part1, 26397);
+        assert_eq!(part2, 288_957);
     }
 }
