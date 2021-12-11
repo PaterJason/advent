d6fa54e426108fb8f3bdc869eaca0a0596369b54
Jason Paterson <jasonbipaterson@gmail.com>
2021-12-11 14:32:36 +0000

    Day 11 2021

---
+++ 2021/src/bin/day11.rs
@@ -0,0 +1,131 @@
+use std::fs;
+
+fn parse_input(input: &str) -> Vec<Vec<i8>> {
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            line.trim()
+                .chars()
+                .map(|ch| ch.to_string().parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn flash_window(i: usize, i_max: usize) -> std::ops::RangeInclusive<usize> {
+    if i == 0 {
+        i..=1
+    } else if i == i_max {
+        (i - 1)..=i_max
+    } else {
+        (i - 1)..=(i + 1)
+    }
+}
+
+fn step(octopodes: &[Vec<i8>]) -> Vec<Vec<i8>> {
+    let mut octs = octopodes.to_vec();
+    let i_max = octs.len() - 1;
+    let j_max = octs[0].len() - 1;
+    for row in &mut octs {
+        for octopus in row {
+            *octopus += 1;
+        }
+    }
+    loop {
+        let mut flashes = 0_u32;
+        for i in 0..=i_max {
+            for j in 0..=j_max {
+                if octs[i][j] > 9 {
+                    octs[i][j] = -1;
+                    flashes += 1;
+                    for ii in flash_window(i, i_max) {
+                        for jj in flash_window(j, j_max) {
+                            if octs[ii][jj] != -1 {
+                                octs[ii][jj] += 1;
+                            };
+                        }
+                    }
+                }
+            }
+        }
+        if flashes == 0 {
+            break;
+        }
+    }
+    for row in &mut octs {
+        for octopus in row {
+            if octopus == &-1 {
+                *octopus = 0;
+            }
+        }
+    }
+    octs
+}
+
+fn part1(octopodes: &[Vec<i8>]) -> u32 {
+    let mut octs: Vec<Vec<i8>> = octopodes.to_vec();
+    let mut flashes = 0_u32;
+    for _ in 0..100 {
+        octs = step(&octs);
+        for row in &octs {
+            for octopus in row {
+                if octopus == &0 {
+                    flashes += 1;
+                }
+            }
+        }
+    }
+    flashes
+}
+
+fn part2(octopodes: &[Vec<i8>]) -> u32 {
+    let mut count = 0_u32;
+    let mut oct = octopodes.to_vec();
+    loop {
+        oct = step(&oct);
+        count += 1;
+        if oct.iter().flatten().all(|&octopus| { octopus == 0 }) {
+            break;
+        }
+    }
+    count
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input/day11").expect("Failed to read input");
+
+    let octopodes = parse_input(&input);
+
+    println!("Part 1: {}", part1(&octopodes));
+    println!("Part 2: {}", part2(&octopodes));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+";
+
+    #[test]
+    fn test_part1() {
+        let octopodes = parse_input(INPUT);
+        assert_eq!(part1(&octopodes), 1656);
+    }
+
+    #[test]
+    fn test_part2() {
+        let octopodes = parse_input(INPUT);
+        assert_eq!(part2(&octopodes), 195);
+    }
+}
