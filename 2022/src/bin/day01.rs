8f8c301268c17785640f66e140a5107cf289f8ee
Jason Paterson <jasonbipaterson@gmail.com>
2022-12-01 13:22:10 +0000

    Add test

--- 2022/src/bin/day01.rs
+++ 2022/src/bin/day01.rs
@@ -42,3 +42,32 @@ fn main() {
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT), 24000);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 45000);
+    }
+}
